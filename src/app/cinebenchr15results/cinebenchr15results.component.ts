8945d52f9c3ba22a504f6d21a9881cbe575bed89
Kit Roed <kit.roed@gmail.com>
2018-05-14 12:10:44 -0500

    CinebenchR15 component not using OnInit.

--- src/app/cinebenchr15results/cinebenchr15results.component.ts
+++ src/app/cinebenchr15results/cinebenchr15results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { MatTableDataSource, MatSort } from '@angular/material';
 import { Router } from '@angular/router';
 
