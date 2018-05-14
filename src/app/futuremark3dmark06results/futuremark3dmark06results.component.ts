4d6691a8b024c79e40cb1d8a8970994728b71361
Kit Roed <kit.roed@gmail.com>
2018-05-14 12:11:18 -0500

    Update Futuremark3dMark06 to use MatTable.

--- src/app/futuremark3dmark06results/futuremark3dmark06results.component.ts
+++ src/app/futuremark3dmark06results/futuremark3dmark06results.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort } from '@angular/material';
 import { Router } from '@angular/router';
 
 import { Futuremark3dMark06Result } from './futuremark3dmark06result';
@@ -9,22 +10,27 @@ import { Futuremark3dMark06ResultService } from './futuremark3dmark06result.serv
   templateUrl: './futuremark3dmark06results.component.html',
   styleUrls: ['./futuremark3dmark06results.component.css']
 })
-export class Futuremark3dMark06ResultsComponent implements OnInit {
-  errorMessage: string;
-  futuremark3dMark06Results: Futuremark3dMark06Result[];
+export class Futuremark3dMark06ResultsComponent implements AfterViewInit {
+  displayedColumns = ['username', 'owner', 'system_name', 'active_revision', 'timestamp',
+  'result_date', 'sm2_score', 'cpu_score', 'sm3_score', 'proxcyon_fps', 'fireflyforest_fps',
+  'cpu1_fps', 'cpu2_fps', 'canyonflight_fps', 'deepfreeze_fps', 'overall_score'];
+
+  dataSource = new Futuremark3dMark06ResultsDataSource(this.futuremark3dMark06ResultService);
+
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private futuremark3dMark06ResultService: Futuremark3dMark06ResultService,
     private router: Router) { }
 
-    getFuturemark3dMark06Results(): void {
-        this.futuremark3dMark06ResultService.getFuturemark3dMark06Results()
-                            .subscribe(
-                                futuremark3dMark06Results => this.futuremark3dMark06Results = futuremark3dMark06Results,
-                                error => this.errorMessage = <any>error);
-    }
+    ngAfterViewInit() {
+      this.dataSource.sort = this.sort;
+  }
+}
 
-  ngOnInit() {
-    this.getFuturemark3dMark06Results();
+export class Futuremark3dMark06ResultsDataSource extends MatTableDataSource<Futuremark3dMark06Result> {
+  constructor(private futuremark3dMark06ResultService: Futuremark3dMark06ResultService) {
+    super();
+    this.futuremark3dMark06ResultService.getFuturemark3dMark06Results().subscribe(d => { this.data = d; })
   }
 }
